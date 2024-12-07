36657af0af358dea6cc1c4a399d7f9536110fda1
unknown <david>
2024-12-07 10:30:34 -0500

    new code

---
+++ Testing02.py
@@ -0,0 +1,9 @@
+
+saludos = 'Hola'
+nombre = input('ingresa tu Full name: ')
+
+print(f"{saludos}, {nombre}! por favor ingresa tu cedula por favor: ")
+cedula = input('CC: ')
+
+print(f"gracias por visitarnos Señor {nombre}, con cedula de ciudadania numer: {cedula}. "
+      f"En pocos minutos sera llamado por el especialista")
